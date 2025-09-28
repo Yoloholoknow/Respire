de0770c6a6e7ad04ef1c20574ab1db3c9125c6f4
Dinofish32 <akarshd32@gmail.com>
2025-09-28 00:55:28 -0400

    added constraints

--- frontend/src/components/profile.jsx
+++ frontend/src/components/profile.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-function patchData(user_id, age, isAsthmatic, hasCOPD) {
+function patchData(user_id, age, isAsthmatic, hasCOPD, otherConditions) {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     myHeaders.append("Accept", "application/json");
     myHeaders.append("authorization", `Bearer ${import.meta.env.VITE_APP_AUTH0_ACCESS_TOKEN}`)
 
     var raw = JSON.stringify({
-        user_metadata: {age, isAsthmatic, hasCOPD}
+        user_metadata: {age, isAsthmatic, hasCOPD, otherConditions}
     });
 
     var requestOptions = {
@@ -30,9 +30,22 @@ const Profile = () => {
     const [age, setAge] = useState("");
     const [isAsthmatic, setIsAsthmatic] = useState(false);
     const [hasCOPD, setHasCOPD] = useState(false);
+    const [otherChecked, setOtherChecked] = useState(false);
+    const [otherConditions, setOtherConditions] = useState("");
 
     // patchData(user.sub, 25, true, true)
+    // useEffect(() => {
+    // if (user) {
+    //   setAge(user.age || ""); // fallback to "" if not set
+    //   setIsAsthmatic(user.isAsthmatic || false);
+    //   setHasCOPD(user.hasCOPD || false);
 
+    //   if (user.otherConditions) {
+    //     setOtherChecked(true);
+    //     setOtherConditions(user.otherConditions);
+    //   }
+    // }
+    // }, [user]);
 
     if (isLoading) {
         return <div>Loading ...</div>;
@@ -40,7 +53,7 @@ const Profile = () => {
 
     const handleSubmit = () => {
         if (user) {
-            patchData(user.sub, age, isAsthmatic, hasCOPD)
+            patchData(user.sub, age, isAsthmatic, hasCOPD, otherConditions);
         }
         setShowPopup(false);
     }
@@ -65,13 +78,16 @@ const Profile = () => {
                 Age:
                 <input
                   type="number"
+                  min="0"
                   value={age}
                   onChange={(e) => setAge(e.target.value)}
                   className="w-full border p-2 rounded mt-1"
                 />
               </label>
+              
+              <span className="text-gray-800 font-semibold mt-4">Conditions:</span>
 
-              <label className="flex items-center mb-2  text-gray-800">
+              <label className="flex items-center mb-2 text-gray-800">
                 <input
                   type="checkbox"
                   checked={isAsthmatic}
@@ -90,6 +106,25 @@ const Profile = () => {
                 />
                 COPD
               </label>
+
+              <label className="flex items-center mb-2 text-gray-800">
+                <input
+                  type="checkbox"
+                  checked={otherChecked}
+                  onChange={(e) => setOtherChecked(e.target.checked)}
+                  className="mr-2"
+                />
+                Other
+              </label>
+              {otherChecked && (
+                <input
+                  type="text"
+                  value={otherConditions}
+                  onChange={(e) => setOtherConditions(e.target.value)}
+                  placeholder="Enter condition"
+                  className="w-full border p-2 rounded mb-4 text-gray-900"
+                />
+              )}
 
               <div className="flex justify-end space-x-2">
                 <button
